f56ae1e4951f51856dc7ac306f3178d1b0f3f2e1
dani <dani.zaghian@gmail.com>
2017-01-25 19:10:27 -0800

    added longitude and latitude to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118042241) do
+ActiveRecord::Schema.define(version: 20170126030705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170118042241) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
